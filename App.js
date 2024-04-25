780507509c96ee6d347b86808e80d1c99f4c74ee
klaus19 <tejaskhartude@gmail.com>
2024-04-25 14:26:45 +0530

    CREATED CUSTOM HEADER

--- App.js
+++ App.js
@@ -11,9 +11,48 @@ const App = () => {
   return (
     <NavigationContainer>
       <stack.Navigator initialRouteName="Home">
-        <stack.Screen name="Home" component={Home} />
-        <stack.Screen name="Detail" component={Detail} />
-        <stack.Screen name="Smile" component={Smile} />
+        <stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'My Home',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <stack.Screen
+          name="Detail"
+          component={Detail}
+          options={{
+            title: 'My Details',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <stack.Screen
+          name="Smile"
+          component={Smile}
+          options={{
+            title: 'My Smile',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
       </stack.Navigator>
     </NavigationContainer>
   );
